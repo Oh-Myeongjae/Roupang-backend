e00c07a40a2af697fe200199bd0489a1fee0b9fa
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-08-28 21:16:37 +0900

    Update ConcurrencyControlAspect.java

--- src/main/java/com/teamsupercat/roupangbackend/common/ConcurrencyControlAspect.java
+++ src/main/java/com/teamsupercat/roupangbackend/common/ConcurrencyControlAspect.java
@@ -21,13 +21,11 @@ public class ConcurrencyControlAspect {
 
     @Before("serviceMethods()")
     public void beforeServiceMethod(JoinPoint joinPoint) throws InterruptedException {
-        System.out.println("메서드 실행이전에 쓰레드 획득");
         semaphore.acquire(); // 스레드 진입을 시도하고, 허용 가능한 수를 넘으면 블록
     }
 
     @After("serviceMethods()")
     public void afterServiceMethod(JoinPoint joinPoint) {
-        System.out.println("메서드 실행이후에 쓰레드 반환");
         semaphore.release(); // 스레드가 메서드 실행을 마치면 Semaphore 해제하여 다음 스레드 진입을 허용
     }
 }
