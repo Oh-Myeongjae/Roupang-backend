b774f5cdda7c8025b3853d5b46a6b62e6a94ced5
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-08-29 23:14:56 +0900

    Update RoupangbackendApplicationTests.java

--- src/test/java/com/teamsupercat/roupangbackend/RoupangbackendApplicationTests.java
+++ src/test/java/com/teamsupercat/roupangbackend/RoupangbackendApplicationTests.java
@@ -3,6 +3,7 @@ package com.teamsupercat.roupangbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//테스트 코드 작성 추가해주면 좋겠다.
 @SpringBootTest
 class RoupangbackendApplicationTests {
 
